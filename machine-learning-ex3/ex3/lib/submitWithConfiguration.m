43bf794dbf351084e6c2acb37988f900f681f52b
姜上 <7777js@gmail.com>
2016-10-16 22:36:08 +0800

    Revert "ex3"
    This reverts commit cb33c02740437849bd823f708c3c3b92ef72c413.

--- machine-learning-ex3/ex3/lib/submitWithConfiguration.m
+++ machine-learning-ex3/ex3/lib/submitWithConfiguration.m
@@ -63,8 +63,7 @@ function response = submitParts(conf, email, token, parts)
   body = makePostBody(conf, email, token, parts);
   submissionUrl = submissionUrl();
   params = {'jsonBody', body};
-  %responseBody = urlread(submissionUrl, 'post', params);
-  [code, responseBody] = system(sprintf('echo jsonBody=%s | curl -k -X POST -d @- %s', body, submissionUrl));
+  responseBody = urlread(submissionUrl, 'post', params);
   response = loadjson(responseBody);
 end
 
